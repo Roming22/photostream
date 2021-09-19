b5d37ab500daa257833f3bbfc7e4f03e3b38c993
Roming22 <roming22@gmail.com>
2021-09-19 18:57:52 -0400

    fix: Images are resized properly

--- src/website/static/js/autorefresh.js
+++ src/website/static/js/autorefresh.js
@@ -20,6 +20,22 @@ function updateImage(data) {
     image_element = document.getElementById("image")
     image_element.src = data.url
     image_element.alt = data.filepath
+    resizeImage(image_element)
+}
+
+function resizeImage(img) {
+    img_ratio = img.naturalWidth / img.naturalHeight
+    container = img.parentElement
+    container_ratio = container.clientWidth / container.clientHeight
+    if (img_ratio > container_ratio) {
+        console.log("Width 100%")
+        img.setAttribute("width", "100%")
+        img.removeAttribute("height")
+    } else {
+        console.log("Height 100%")
+        img.setAttribute("height", "100%")
+        img.removeAttribute("width")
+    }
 }
 
 loadImage()
