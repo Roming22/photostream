f25eaf0eb83a48394540f710fb86367387e46399
Roming22 <roming22@gmail.com>
2021-10-09 11:49:19 -0400

    feat(image): Multi-arch builds

--- tools/releasing/build.sh
+++ tools/releasing/build.sh
@@ -6,30 +6,22 @@ set -o pipefail
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 PROJECT_DIR="$(realpath "${SCRIPT_DIR}/../..")"
 
-# Generate a new version
-if [[ -n "${IMAGE_REPOSITORY_USER}" ]]; then
-    python tools/releasing/version.py
-    VERSION="$(python "${PROJECT_DIR}/src/website/__version__.py")"
-else
-    VERSION="latest"
+IMAGE_REPOSITORY_HOST="k3d-registry.localhost"
+if ping -c1 -q "$IMAGE_REPOSITORY_HOST" >/dev/null 2>&1; then
+    IMAGE_REPOSITORY_USER="${IMAGE_REPOSITORY_HOST}:5000/skwr/web"
 fi
-# Build the image
-echo "Release: ${VERSION}"
+VERSION="latest"
 DOCKERFILE="${PROJECT_DIR}/tools/tooling/container/Dockerfile"
 TAG="photostream:latest"
 TARGET="release"
+
 docker build --file "${DOCKERFILE}" --tag "${TAG}" --target "${TARGET}" "${PROJECT_DIR}"
 
+# Print the tag
 if [[ -n "${IMAGE_REPOSITORY_USER}" ]]; then
     docker tag "${TAG}" "${IMAGE_REPOSITORY_USER}/photostream:${VERSION}"
     echo "${IMAGE_REPOSITORY_USER}/photostream:${VERSION}"
 else
-    IMAGE_REPOSITORY_USER="k3d-registry.localhost"
-    if ping -c1 -q "$IMAGE_REPOSITORY_USER" >/dev/null 2>&1; then
-        IMAGE_REPOSITORY_USER="${IMAGE_REPOSITORY_USER}:5000/skwr/web"
-        docker tag "${TAG}" "${IMAGE_REPOSITORY_USER}/photostream:${VERSION}"
-    else
-        echo "[WARNING] IMAGE_REPOSITORY_USER is not set, image can't be pushed without being retagged";
-    fi
+    echo "[WARNING] IMAGE_REPOSITORY_USER is not set, image can't be pushed without being retagged" >&2;
     echo "$TAG"
 fi
