23363c29cdb2e52068526e9838c907e6a403880e
Roming22 <roming22@gmail.com>
2026-09-19 08:05:33 -0400

    feat(releasing): Publish images to GitHub Container Registry

--- tools/releasing/upload.sh
+++ tools/releasing/upload.sh
@@ -7,6 +7,7 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 PROJECT_DIR="$(realpath "${SCRIPT_DIR}/../..")"
 python tools/releasing/version.py
 VERSION="$(python "${PROJECT_DIR}/src/website/__version__.py")"
+IMAGE_REPOSITORY_USER="${IMAGE_REPOSITORY_USER,,}"
 
 if [[ -z "${IMAGE_REPOSITORY_USER}" ]]; then
     IMAGE_REPOSITORY="k3d-registry.localhost"
@@ -20,7 +21,7 @@ if [[ -z "${IMAGE_REPOSITORY_USER}" ]]; then
         exit 1
     fi
 else
-    IMAGE="${IMAGE_REPOSITORY_USER}/photostream:${VERSION}"
+    IMAGE="${IMAGE_REPOSITORY_URL}/${IMAGE_REPOSITORY_USER}/photostream:${VERSION}"
 fi
 
 # Make sure that the credentials have been defined
@@ -59,7 +60,7 @@ if [[ "${GITHUB_REF}" = refs/heads/release/* && "${GITHUB_EVENT_NAME}" == "push"
     PLATFORM="linux/arm64/v8,linux/amd64"
     TARGET="release"
     docker login --password "${IMAGE_REPOSITORY_TOKEN}" --username "${IMAGE_REPOSITORY_USER}" "${IMAGE_REPOSITORY_URL}"
-    for TAG in ${IMAGE} "${IMAGE_REPOSITORY_USER}/photostream:latest"; do
+    for TAG in ${IMAGE} "${IMAGE_REPOSITORY_URL}/${IMAGE_REPOSITORY_USER}/photostream:latest"; do
         docker buildx build --file "${DOCKERFILE}" --platform "${PLATFORM}" --push --tag "${TAG}" --target "${TARGET}" "${PROJECT_DIR}"
     done
     echo "Uploaded ${TAG}"
